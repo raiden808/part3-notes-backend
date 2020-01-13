d0f2504fcb54fa90d0406be7d2def99600a50f25
raiden808 <raiden.nalasa@gmail.com>
2020-01-14 05:38:59 +0800

    added build

---
+++ build/precache-manifest.8c9b0a2e825b8a380b5302b9c9de819e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fc103c1fc0003ce8a0c1dea16fdae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f89478f6bfdbb8f6dd50",
+    "url": "/static/css/main.721623ee.chunk.css"
+  },
+  {
+    "revision": "0ce51292aec41b5a94e8",
+    "url": "/static/js/2.5a6e94ad.chunk.js"
+  },
+  {
+    "revision": "f89478f6bfdbb8f6dd50",
+    "url": "/static/js/main.53d5d67d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
