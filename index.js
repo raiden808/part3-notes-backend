9a82eb99abfc3a90f782c04f178f26a5c3660fc5
raiden808 <raiden.nalasa@gmail.com>
2020-01-13 14:01:24 +0800

    refactor heroku

--- index.js
+++ index.js
@@ -104,6 +104,12 @@ app.delete('/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
