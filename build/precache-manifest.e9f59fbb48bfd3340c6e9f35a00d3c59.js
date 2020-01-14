7efac5cc0c8fd28a198122f3620016ecee30627f
raiden808 <raiden.nalasa@gmail.com>
2020-01-14 12:17:05 +0800

    uibuild

---
+++ build/precache-manifest.e9f59fbb48bfd3340c6e9f35a00d3c59.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d7d28fba29ffcd11cf4b2484fb2621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a6b9d7d00ee090eb90",
+    "url": "/static/js/2.1fdc582c.chunk.js"
+  },
+  {
+    "revision": "a9891f0c067f9305d15f",
+    "url": "/static/js/main.351a44ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52806ca004b27c96530745215c8c1be3.js"
+  "/precache-manifest.e9f59fbb48bfd3340c6e9f35a00d3c59.js"
 );
 
 self.addEventListener('message', (event) => {
