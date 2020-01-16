b55142fc124fc3f69a2837d98ac102f0d7e63b7d
raiden808 <raiden.nalasa@gmail.com>
2020-01-16 13:36:30 +0800

    impelment model

---
+++ models/note.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to',url)
+
+mongoose.connect(url,{useNewUrlParser:true})
+    .then(result =>{
+        console.log('Connected to MongoDB')
+    })
+    .catch((error)=>{
+        console.log("error connecting to MongoDB", error.message)
+    })
+
+/**
+ * MongoDB document structure
+ */
+const noteSchema = mongoose.Schema({
+    content:String,
+    date:Date,
+    important:Boolean,
+})
+
+/**
+ * Modify Schema output
+ */
+noteSchema.set('toJSON',{
+    transform:(document,returnedObject) =>{
+        delete returnedObject._id,
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
