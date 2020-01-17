8e63f0e2da868726a11036700f7bdddd48814b2d
raiden808 <raiden.nalasa@gmail.com>
2020-01-17 12:40:30 +0800

    add unified topology

--- models/note.js
+++ models/note.js
@@ -10,7 +10,7 @@ console.log('connecting to',url)
 /**
  * Connection verification
  */
-mongoose.connect(url,{useNewUrlParser:true})
+mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology: true})
     .then(result =>{
         console.log('Connected to MongoDB')
     })
