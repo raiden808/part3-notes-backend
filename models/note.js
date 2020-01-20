3b5063a6905c141dd7d40669082da04fc8819544
raiden808 <raiden.nalasa@gmail.com>
2020-01-20 10:04:12 +0800

    refactor schema

--- models/note.js
+++ models/note.js
@@ -22,10 +22,18 @@ mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology: true})
 
 /**
  * MongoDB document structure
+ * can also add restrictions
  */
 const noteSchema = mongoose.Schema({
-    content:String,
-    date:Date,
+    content:{
+        type:String,
+        minlength:5,
+        required:true
+    },
+    date:{
+        type:Date,
+        required: true
+    },
     important:Boolean,
 })
 
