b6be9dc590cc1be48a1966623ea784b4ee90f856
raiden808 <raiden.nalasa@gmail.com>
2020-01-21 13:40:21 +0800

    refactor using eslint

--- models/note.js
+++ models/note.js
@@ -12,43 +12,43 @@ console.log('connecting to',url)
 /**
  * Connection verification
  */
-mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology: true})
-    .then(result =>{
-        console.log('Connected to MongoDB')
-    })
-    .catch((error)=>{
-        console.log("error connecting to MongoDB", error.message)
-    })
+mongoose.connect(url,{ useNewUrlParser:true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
 
 /**
  * MongoDB document structure
  * can also add restrictions like "required" & "minlength"
  */
 const noteSchema = mongoose.Schema({
-    content: {
-        type: String,
-        minlength: 5,
-        required: true
-    },
-    date: { 
-        type: Date,
-        required: true
-    },
-    important: Boolean
+  content: {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean
 })
 
 /**
  * Modify Schema output
  */
 noteSchema.set('toJSON',{
-    transform:(document,returnedObject) =>{
-        /**
+  transform:(document,returnedObject) => {
+    /**
          * displays proper id
          */
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id,
-        delete returnedObject.__v
-    }
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id,
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
---
+++ utils/config.js
@@ -0,0 +1,9 @@
+require('dotnev').config()
+
+let PORT        = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
+
+module.exports = {
+  MONGODB_URI,
+  PORT
+}
