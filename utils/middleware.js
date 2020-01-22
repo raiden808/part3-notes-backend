ae2050081622fe557b776810acd27c2f45a8b653
raiden808 <raiden.nalasa@gmail.com>
2020-01-22 08:50:02 +0800

    update middlewares

---
+++ utils/middleware.js
@@ -0,0 +1,39 @@
+/**
+ * Custom request handler middleware
+ */
+const requestLogger = (request, response, next) => {
+    console.log('Method: ', request.method),
+    console.log('Path: ', request.path)
+    console.log('Body: ', request.body)
+    console.log('---')
+
+    /**
+     * Proceeds to next middleware
+     */
+    next()
+}
+
+/**
+ * If API does not exist.
+*/
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unkown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if(error.name === 'CastError' && error.kind === 'ObjectId'){
+        return response.status(400).send({ error:'malformatted id' })
+    } else if (error.name === 'ValidationError'){
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+module.exports = {
+    requestLogger,
+    unknownEndpoint,
+    errorHandler
+}
