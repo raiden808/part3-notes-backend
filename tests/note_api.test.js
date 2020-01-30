a57ec2f43f713d17b7da3b1bb21a80b2462ec309
raiden808 <raiden.nalasa@gmail.com>
2020-01-30 13:01:36 +0800

    jest update

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -16,6 +16,19 @@ test('notes are returned as json', async () => {
         .expect(200)
         .expect('Content-Type', /application\/json/)
 })
+
+test('there are four notes', async () => {
+    const response = await api.get('/api/notes')
+
+    expect(response.body.length).toBe(4)
+})
+
+test('the first note is about HTTP methods', async () => {
+    const response = await api.get('/api/notes')
+
+    expect(response.body[0].content).toBe('HTML is easy')
+})
+
 /**
  * Once all test() is done
  */
