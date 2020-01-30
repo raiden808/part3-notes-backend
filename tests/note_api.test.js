87f8813c1e2daf71218cbdf7bc9e77084f03278e
raiden808 <raiden.nalasa@gmail.com>
2020-01-30 12:55:20 +0800

    jest update

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -2,15 +2,23 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
+/**
+ * Enables test() to make http request to the backend
+ */
 const api = supertest(app)
 
-test('notes are returned as json', async() => {
+/**
+ * Makes sure proper get response is recieve
+ */
+test('notes are returned as json', async () => {
     await api
         .get('/api/notes')
         .expect(200)
         .expect('Content-Type', /application\/json/)
 })
-
+/**
+ * Once all test() is done
+ */
 afterAll(() => {
     mongoose.connection.close()
 })
