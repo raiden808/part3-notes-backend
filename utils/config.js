1a0608593a2dbfe93b1ce495e66f423e0f0fc6e5
raiden808 <raiden.nalasa@gmail.com>
2020-01-30 13:24:26 +0800

    update jest

--- utils/config.js
+++ utils/config.js
@@ -5,6 +5,8 @@ let MONGODB_URI = process.env.MONGODB_URI
 
 /**
  * Determines mongo url to be used.
+ * set up by cross-env package to allow 
+ * different db for production
  */
 if (process.env.NODE_ENV === 'test') {
     MONGODB_URI = process.env.TEST_MONGODB_URI
