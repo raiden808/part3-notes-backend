1342eae2e740fb0d9c8335303add9460b5a93158
raiden808 <raiden.nalasa@gmail.com>
2020-01-30 13:20:45 +0800

    jest update

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,13 @@ require('dotenv').config()
 let PORT        = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
+/**
+ * Determines mongo url to be used.
+ */
+if (process.env.NODE_ENV === 'test') {
+    MONGODB_URI = process.env.TEST_MONGODB_URI
+}
+
 module.exports = {
     MONGODB_URI,
     PORT
