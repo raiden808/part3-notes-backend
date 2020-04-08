05e6fb4ea32ebfd287b360c8ab76a582f7dc6f6d
raiden808 <raiden.nalasa@gmail.com>
2020-04-08 09:20:49 +0800

    added user tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -13,6 +13,12 @@ const api = supertest(app)
  */
 const Note = require('../models/note')
 
+/**
+ * User API testing
+ */
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
 beforeEach(async () => {
     /**
      * Clear the database at the beggining
@@ -135,6 +141,40 @@ describe('deletion of a note', () => {
         expect(contents).not.toContain(noteToDelete.content)
     })
 
+})
+
+describe('when there is initially one user at db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })    
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
 })
 
 /**
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,31 +1,40 @@
 const Note = require('../models/note')
+const User = require('../models/user')
 
 const initialNotes = [
-  {
-    content: 'HTML is easy',
-    important: false,
-    date: new Date(),
-  },
-  {
-    content: 'Browser can execute only Javascript',
-    important: true,
-    date: new Date(),
-  }
+    {
+        content: 'HTML is easy',
+        important: false,
+        date: new Date(),
+    },
+    {
+        content: 'Browser can execute only Javascript',
+        important: true,
+        date: new Date(),
+    }
 ]
 
 const nonExistingId = async () => {
-  const note = new Note({ content: 'willremovethissoon' })
-  await note.save()
-  await note.remove()
+    const note = new Note({ content: 'willremovethissoon' })
+    await note.save()
+    await note.remove()
 
-  return note._id.toString()
+    return note._id.toString()
 }
 
 const notesInDb = async () => {
-  const notes = await Note.find({})
-  return notes.map(note => note.toJSON())
+    const notes = await Note.find({})
+    return notes.map(note => note.toJSON())
+}
+
+const usersIndDb = async () => {
+    const users = await users.find({})
+    return users.map(u => u.toJSON())
 }
 
 module.exports = {
-  initialNotes, nonExistingId, notesInDb
+    initialNotes, 
+    nonExistingId, 
+    notesInDb,
+    usersIndDb
 }
